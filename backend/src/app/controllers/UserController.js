7140cb20be1f1511d88974fc85b13551d69aac44
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-02-17 17:22:02 -0300

    starter UserController

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,22 @@
+/* eslint-disable class-methods-use-this */
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const UserExist = await User.findOne({ where: { email: req.body.email } });
+
+    if (UserExist) {
+      return res.status(400).json({ error: 'User alredy exist!' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
