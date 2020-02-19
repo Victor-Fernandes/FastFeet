032da943f972908d43187bf2baba04e188dd8414
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-02-18 23:58:34 -0300

    validaçao dos campos de entrada, migration-recipients

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,14 +1,34 @@
 /* eslint-disable class-methods-use-this */
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+      confirmPassword: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const UserExist = await User.findOne({ where: { email: req.body.email } });
 
     if (UserExist) {
       return res.status(400).json({ error: 'User alredy exist!' });
     }
 
+    const { password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: 'Passwords do not match!' });
+    }
     const { id, name, email } = await User.create(req.body);
 
     return res.json({
@@ -19,11 +39,53 @@ class UserController {
   }
 
   async update(req, res) {
-    const { email, oldPassword } = req.body;
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
 
-    // termina update
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
 
-    return res.json();
+    const { email, oldPassword, password } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const UserExist = await User.findOne({ where: { email } });
+
+      if (UserExist) {
+        return res.status(400).json({ error: 'User alredy exist!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Oldpassword does not match' });
+    }
+
+    if (oldPassword === password) {
+      return res.status(401).json({
+        error: 'The new password cannot be the same as the old password',
+      });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
