f21f1084d2a7fdbe6444244020366a2d2f784d3c
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-03-03 15:12:56 -0300

    rotas para o CRUD de Deliveryman

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientsController from './app/controllers/RecipientsController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -13,7 +14,15 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+// recipients routes
 routes.post('/recipients', RecipientsController.store);
 routes.put('/recipients/:id', RecipientsController.update);
+
+// Deliveryman routes
+routes.post('/deliverymans', DeliverymanController.store);
+routes.get('/deliverymans', DeliverymanController.index);
+routes.put('/deliverymans/:id', DeliverymanController.update);
+routes.delete('/deliverymans/:id', DeliverymanController.delete);
 
 export default routes;
