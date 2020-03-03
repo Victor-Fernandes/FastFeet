31c66abeb899794dc7b7c2ff702ccd4fee56ae6d
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-03-03 07:38:28 -0300

    adicionando tratamento de exeptions

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,43 @@
 import express from 'express';
-
+import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middleware();
     this.routes();
+    this.exceptionHandler();
   }
 
   middleware() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // middleware de tratamento de exceptions
+    this.use.server(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJson();
+
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal server erro.' });
+    });
   }
 }
 
