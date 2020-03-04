839daf0aa4824fbed22255f03743fbda32e6cf25
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-03-03 23:18:42 -0300

    terminando a rota de upload

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import configMulter from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientsController from './app/controllers/RecipientsController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(configMulter);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -26,8 +30,5 @@ routes.put('/deliverymans/:id', DeliverymanController.update);
 routes.delete('/deliverymans/:id', DeliverymanController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
-/**
- * Fazer configuração do multer
- */
 
 export default routes;
