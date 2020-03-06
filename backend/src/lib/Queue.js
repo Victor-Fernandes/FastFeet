71ead17a13e9a4f45090e6e60247c3cd0de75099
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-03-06 20:22:59 -0300

    processar filas, processar jobs em background e monitora erros nas filas

---
+++ backend/src/lib/Queue.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+/* eslint-disable class-methods-use-this */
+import Bee from 'bee-queue';
+
+import redisConfig from '../config/redis';
+import CreateMail from '../app/jobs/CreateMail';
+
+const jobs = [CreateMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  // bee connecta com o redis, que armazena e recupera valores
+  // handle -> processa as filas
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  // processa os jobs em background
+  processoQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.on('failed', this.handleFailure).process(handle);
+    });
+  }
+
+  // monitora erros nas filas
+  handleFailure(job, error) {
+    console.log(`Queue ${job.queue.name}: Failed`, error);
+  }
+}
+
+export default new Queue();
