33d9223c78aedff4ccf1d1842225f35205904b93
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-03-14 19:44:47 -0300

    listando encomendas entregues e em aberto

--- backend/src/app/controllers/DeliveryController.js
+++ backend/src/app/controllers/DeliveryController.js
@@ -1,11 +1,28 @@
 /* eslint-disable no-dupe-keys */
 /* eslint-disable class-methods-use-this */
+import { Op } from 'sequelize';
 import Orders from '../models/Orders';
 
 class DeliveryController {
   async index(req, res) {
-    const { page = 1 } = req.query;
+    const { page = 1, finished = true } = req.query;
 
+    if (finished) {
+      const delivery = await Orders.findAll({
+        where: {
+          deliveryman_id: req.params.id,
+          end_date: {
+            [Op.ne]: null,
+            // verifica se é diferente de null, olhar manual do sequelize
+            // https://sequelize.org/v4/manual/tutorial/querying.html | operators
+          },
+        },
+        order: ['id'],
+        limit: 20,
+        offset: (page - 1) * 20,
+      });
+      return res.json(delivery);
+    }
     const delivery = await Orders.findAll({
       where: {
         deliveryman_id: req.params.id,
